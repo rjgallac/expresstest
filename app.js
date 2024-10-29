3b3aa615ff970878259fb1e5a5520ca09e5dd9c1
rjgallac <rjgallac@hotmail.com>
2024-10-29 14:59:52 +0000

    edit view

--- app.js
+++ app.js
@@ -70,6 +70,13 @@ app.delete('/items/:id', (req,res) =>{
     })
 })
 
+app.put('/items/:id', (req,res) =>{
+    const id = req.params.id;
+    Item.findByIdAndUpdate(id, req.body).then(result => {
+        res.json({msg: 'Updated'});
+    })
+})
+
 app.use((req,res) => {
     res.render('error');
 });
